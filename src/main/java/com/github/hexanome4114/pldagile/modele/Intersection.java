ba43e50f0e565f8b0ecff85b3d8b7a1e242d4333
Foutete <francois.foltete@insa-lyon.fr>
2022-10-28 09:05:36 +0200

    Set l'id dans le controlleur d'Intersection

--- src/main/java/com/github/hexanome4114/pldagile/modele/Intersection.java
+++ src/main/java/com/github/hexanome4114/pldagile/modele/Intersection.java
@@ -7,6 +7,7 @@ public final class Intersection extends MapPoint {
     private String id;
     public Intersection(String id, final double latitude, final double longitude) {
         super(latitude, longitude);
+        this.id = id;
     }
 
     public String getId() {
