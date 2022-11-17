dbd69ed20a01a73eaf9b0f75b1d9608ec5ea3fa2
Adrien <50359008+AdrienLeem@users.noreply.github.com>
2022-11-17 16:56:26 +0100

    Ajout de l'état tournée (#25)

--- src/main/java/com/github/hexanome4114/pldagile/controleur/EtatLivraison.java
+++ src/main/java/com/github/hexanome4114/pldagile/controleur/EtatLivraison.java
@@ -1,8 +1,23 @@
 package com.github.hexanome4114.pldagile.controleur;
 
 
+/**
+ * Etat de l'application lorsque des livraisons ont été ajoutées.
+ * Il est possible de rajouter des livraisons ou en supprimer,
+ * l'état livraison est conservé même si il n'y a aucune livraison.
+ * Il est possible de passer à l'état tournée si l'on clique sur
+ * le bouton calculer les tournées, il est aussi possible de passer
+ * à l'état plan chargé si l'on recharge un plan, les livraisons sont
+ * alors réinitialiser. Si l'on charge des livraisons depuis un fichier,
+ * l'état est conservé mais les livraisons sont réinitialiser
+ */
 public final class EtatLivraison implements Etat {
 
+    /**
+     * Passage de l'état livraison à l'état plan chargé
+     * lorsque l'on charge un nouveau plan.
+     * @param c le controleur qui change d'état
+     */
     @Override
     public void chargerPlan(final Controleur c) {
         c.getComboBoxLivreur().setValue(null);
@@ -23,5 +38,42 @@ public final class EtatLivraison implements Etat {
                         + " en cliquant sur la carte."
         );
         c.setEtatCourant(c.getEtatPlanCharge());
+    }
+
+    /**
+     * Réinitialise l'état livraison
+     * lorsque l'on charge de nouvelles livraisons.
+     * @param c le controleur qui change d'état
+     */
+    @Override
+    public void chargerLivraison(final Controleur c) {
+        c.getSauvegarderLivraisonsBouton().setDisable(false);
+        c.getCalculerTourneeBouton().setDisable(false);
+        c.getListeDeCommandes().reinitialiser();
+        c.getAnnulerBouton().setDisable(true);
+        c.setEtatCourant(c.getEtatLivraison());
+    }
+
+    /**
+     * Passage de l'état livraison à l'état tournée
+     * lorsque l'on calcule les tournées.
+     * @param c le controleur qui change d'état
+     */
+    @Override
+    public void calculerTournee(final Controleur c) {
+        c.getComboBoxLivreur().setValue(null);
+        c.getComboBoxLivreur().setDisable(false);
+        c.getComboBoxFenetreDeLivraison().setValue(null);
+        c.getComboBoxFenetreDeLivraison().setDisable(false);
+        c.getAjouterLivraisonBouton().setDisable(true);
+        c.getCalculerTourneeBouton().setDisable(true);
+        c.getChargerLivraisonBouton().setDisable(true);
+        c.getInstructionLabel().setText(
+                "Pour ajouter une livraison, veuillez sélectionner un livreur,"
+                        + " une fenêtre horaire, une adresse de livraison"
+                        + " en cliquant sur la carte et la place de la"
+                        + " nouvelle livraison dans la tournée."
+        );
+        c.setEtatCourant(c.getEtatTournee());
     }
 }
---
+++ src/main/java/com/github/hexanome4114/pldagile/controleur/EtatTournee.java
@@ -0,0 +1,40 @@
+package com.github.hexanome4114.pldagile.controleur;
+
+
+/**
+ * Etat de l'application lorsque l'on a calculé les tournées.
+ * Il est seulement possible de revenir à l'état plan chargé
+ * si l'on recharge un nouveau plan.
+ */
+public final class EtatTournee implements Etat {
+
+    /**
+     * Passage de l'état tournée à l'état plan chargé
+     * lorsque l'on charge un nouveau plan.
+     * @param c le controleur qui change d'état
+     */
+    @Override
+    public void chargerPlan(final Controleur c) {
+        c.getComboBoxLivreur().setDisable(false);
+        c.getComboBoxLivreur().setValue(null);
+        c.getComboBoxFenetreDeLivraison().setDisable(false);
+        c.getComboBoxFenetreDeLivraison().setValue(null);
+        c.getComboBoxAdresse().setValue(null);
+        c.getTableauLivraison().getItems().clear();
+        c.getTableauLivraison().getSelectionModel().clearSelection();
+        c.getAjouterLivraisonBouton().setDisable(true);
+        c.getSupprimerLivraisonBouton().setDisable(true);
+        c.getSauvegarderLivraisonsBouton().setDisable(true);
+        c.getChargerLivraisonBouton().setDisable(false);
+        c.getCalculerTourneeBouton().setDisable(true);
+        c.getAfficherPointsCheckBox().setSelected(false);
+        c.getListeDeCommandes().reinitialiser();
+        c.getAnnulerBouton().setDisable(true);
+        c.getInstructionLabel().setText(
+                "Pour ajouter une livraison, veuillez sélectionner un livreur,"
+                        + " une fenêtre horaire et une adresse de livraison"
+                        + " en cliquant sur la carte."
+        );
+        c.setEtatCourant(c.getEtatPlanCharge());
+    }
+}
